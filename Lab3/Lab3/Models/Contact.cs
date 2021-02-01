4a4118155455d71b89eed0b227453d3a06186830
Calvin Pierce <calvinpierce99@gmail.com>
2021-02-01 09:52:00 -0500

    Work in progress Comp 2139 Lab 3

---
+++ Lab3/Lab3/Models/Contact.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab3.Models
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+
+        [Required(ErrorMessage="Please enter a first name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter a last name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter a phone number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please enter an email address")]
+        public string Email { get; set; }
+
+        public string Organization { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Range(1, 100000, ErrorMessage = "Please select a category.")]
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+        public string Slug => FirstName?.Replace(" ", "-").ToLower() +
+            "-" + LastName?.Replace(" ", "-").ToLower();
+    }
+}
---
+++ Lab3/Lab3/Models/ContactContext.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Lab3.Models
+{
+    public class ContactContext : DbContext
+    {
+
+        public ContactContext(DbContextOptions<ContactContext> options)
+            : base(options)
+        { }
+
+        DbSet<Contact> Contacts { get; set; }
+
+        DbSet<Category> Categories { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = 1, Name = "Family" },
+                new Category { CategoryId = 2, Name = "Friend" },
+                new Category { CategoryId = 3, Name = "Work" },
+                new Category { CategoryId = 4, Name = "Other" }
+            );
+
+            modelBuilder.Entity<Contact>().HasData(
+               new Contact { 
+                   ContactId = 1, 
+                   FirstName = "Bruce",
+                   LastName = "Wayne",
+                   Phone = "416-123-4567",
+                   Email = "bruce.wayne@domain.com",
+                   CategoryId = 1,
+                   DateAdded = DateTime.Now
+               },
+
+                new Contact
+                {
+                    ContactId = 2,
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Phone = "647-123-4567",
+                    Email = "peter.parker@gmail.com",
+                    CategoryId = 2,
+                    DateAdded = DateTime.Now
+                },
+
+                 new Contact
+                 {
+                     ContactId = 3,
+                     FirstName = "Bruce",
+                     LastName = "Banner",
+                     Phone = "905-123-4567",
+                     Email = "bruce.banner@hotmail.com",
+                     CategoryId = 3,
+                     DateAdded = DateTime.Now
+                 }
+            );
+
+        }
+
+    }
+}
